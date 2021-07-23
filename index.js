5e834b5a328fe69f028547e6f155d17495b31c89
Erica <ericarojo692@yahoo.com>
2021-07-23 16:06:48 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+//document.querySelector("main").remove();
+
+
+
+ //=> Returns the element on the page with an id attribute equal to "firstHeading"  
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Erica is the champion"
+
+
+newHeader.id("Erica is the champion");
+
+document.body.appendChild('h1');
+
+
